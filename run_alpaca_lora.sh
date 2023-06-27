5b986ef574b655a047044214dbc2b8e83c0e524a
v-lbei <FAREAST.v-lbei@microsoft.com>
2023-06-27 11:23:03 +0000

    commit

--- run_alpaca_lora.sh
+++ run_alpaca_lora.sh
@@ -1,7 +1,8 @@
 gpu=0
 # gpu=0,1,2,3,4,5,6,7
 # finetune_checkpoint="alpaca-lora/"
-finetune_checkpoint=/home/v-lbei/alpaca-lora/625_7b
+finetune_checkpoint=Angainor/alpaca-lora-13b
 data_file=""
 export CUDA_VISIBLE_DEVICES=$gpu
-python3 generate_my.py --load_8bit --base_model chavinlo/alpaca-native --lora_weights $finetune_checkpoint
+# python3 generate_my.py --load_8bit --base_model ../alpaca13b --lora_weights $finetune_checkpoint
+python3 generate_my.py --load_8bit --base_model 'decapoda-research/llama-13b-hf' --lora_weights $finetune_checkpoint
