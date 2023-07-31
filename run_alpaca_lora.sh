b6b404f340b89b518063fa65d5d6b0f4e64fe0d6
v-lbei <FAREAST.v-lbei@microsoft.com>
2023-07-31 07:28:54 +0000

    udpate the gitignore

--- run_alpaca_lora.sh
+++ run_alpaca_lora.sh
@@ -1,8 +1,12 @@
 gpu=0
 # gpu=0,1,2,3,4,5,6,7
 # finetune_checkpoint="alpaca-lora/"
-finetune_checkpoint=Angainor/alpaca-lora-13b
+# finetune_checkpoint=output/llama-13b-fixed-instruction-new
+finetune_checkpoint=output/wmt21_50K
 data_file=""
 export CUDA_VISIBLE_DEVICES=$gpu
 # python3 generate_my.py --load_8bit --base_model ../alpaca13b --lora_weights $finetune_checkpoint
-python3 generate_my.py --load_8bit --base_model 'decapoda-research/llama-13b-hf' --lora_weights $finetune_checkpoint
+# python3 generate_my.py --load_8bit --base_model 'decapoda-research/llama-7b-hf' 
+
+python3 generate_batch.py --load_8bit --base_model ../alpaca13b --lora_weights $finetune_checkpoint --batch_size 4
+# python3 generate_batch.py --load_8bit --base_model 'decapoda-research/llama-7b-hf' --lora_weights 'tloen/alpaca-lora' --batch_size 4
